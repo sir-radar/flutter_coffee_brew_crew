cbb71d8bf8d166e4bb72d882342d48855e587f42
MAC <nwokikesamson@gmail.com>
2019-12-08 02:21:35 +0100

    register functionality

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -2,7 +2,9 @@ import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
-  Register({Key key}) : super(key: key);
+  final Function toggleView;
+
+  Register({this.toggleView});
 
   @override
   _RegisterState createState() => _RegisterState();
@@ -10,52 +12,80 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
   //text filed state
   String email = '';
   String password = '';
+  String error = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Colors.brown[100],
-       appBar: AppBar(
-         backgroundColor: Colors.brown[400],
-         elevation: 0,
-         title: Text('Sign up to Brew Crew'),
-       ),
-       body: Container(
-         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-         child: Form(
-           child: Column(
-             children: <Widget>[
-               SizedBox(height: 20),
-               TextFormField(
-                 onChanged: (val){
-                   setState(() => email = val);
-                 },
-               ),
-               SizedBox(height: 20),
-               TextFormField(
-                 obscureText: true,
-                 onChanged: (val){
-                   setState(() => password = val);
-                 },
-               ),
-               SizedBox(height: 20,),
-               RaisedButton(
-                 color: Colors.pink[400],
-                 child: Text(
-                   'Register',
-                   style: TextStyle(color: Colors.white),
-                 ),
-                 onPressed: () async {
-                   print(email);
-                   print(email);
-                 },
-               )
-             ],
-           ),
-         )
-       ),
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0,
+        title: Text('Sign up to Brew Crew'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Sign in'),
+            onPressed: () {
+              widget.toggleView();
+            },
+          )
+        ],
+      ),
+      body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20),
+                TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  validator: (val) =>
+                      val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() => error = 'Credencials invalid');
+                      }
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,7 +2,10 @@ import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  SignIn({Key key}) : super(key: key);
+
+  final Function toggleView;
+
+  SignIn({this.toggleView});
 
   @override
   _SignInState createState() => _SignInState();
@@ -23,6 +26,15 @@ class _SignInState extends State<SignIn> {
          backgroundColor: Colors.brown[400],
          elevation: 0,
          title: Text('Sign in to Brew Crew'),
+         actions: <Widget>[
+           FlatButton.icon(
+             icon: Icon(Icons.person),
+             label: Text('Register'),
+             onPressed: (){
+                widget.toggleView();
+             },
+           )
+         ],
        ),
        body: Container(
          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
