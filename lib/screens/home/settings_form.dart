c9cffe3833b9941fe5caff6792e1b891f7c4ca07
MAC <nwokikesamson@gmail.com>
2019-12-09 16:04:37 +0100

    added drop down button

---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,61 @@
+import 'package:brew_crew/shared/constants.dart';
+import 'package:flutter/material.dart';
+
+
+class SettingsForm extends StatefulWidget {
+  SettingsForm({Key key}) : super(key: key);
+
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+  
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+       child: Column(children: <Widget>[
+         Text(
+           'Update your brew settings',
+           style: TextStyle(fontSize: 18),
+         ),
+         SizedBox(height: 20,),
+         TextFormField(
+           decoration: textInputDecoration,
+           validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+           onChanged: (val) => setState(() => _currentName = val),
+         ),
+         SizedBox(height: 20),
+         DropdownButtonFormField(
+           decoration: textInputDecoration,
+           value: _currentSugars ?? '0',
+           items: sugars.map((sugar){
+             return DropdownMenuItem(
+               value: sugar,
+               child: Text('$sugar sugars')
+             );
+           }).toList(),
+           onChanged: (val) => setState(() => _currentSugars = val),
+         ),
+         RaisedButton(
+           color: Colors.pink[400],
+           child: Text(
+             'Update',
+             style: TextStyle(color: Colors.white)
+           ),
+           onPressed: () async {
+             
+           },
+         )
+       ],),
+    );
+  }
+}
